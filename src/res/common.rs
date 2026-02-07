00a327ecb825574fde409c0495cb445d8fe0d553
frtian <osk9ehdms01@gmail.com>
2026-02-07 00:06:54 -0300

    feat: atualiza gerenciamento de rng e resources/events globais

--- src/res/common.rs
+++ src/res/common.rs
@@ -1,12 +1,23 @@
 use bevy::prelude::*;
-use rand::{Rng, RngCore};
+use rand_chacha::ChaCha8Rng;
+use rand::prelude::*;
 
-// COMMON COLORS
+// COMMON RESOURCES
 pub struct Colors;
 impl Colors {
-    pub const PRESSED: Color = Color::srgb(0.1, 0.1, 0.1);
+    pub const PRESSED: Color = Color::srgb(0.15, 0.15, 0.15);
     pub const DARK_GRAY: Color = Color::srgb(0.2, 0.2, 0.2);
-    pub const LIGHT_GRAY: Color = Color::srgb(0.8, 0.8, 0.8);
+    pub const LIGHT_GRAY: Color = Color::srgb(0.25, 0.25, 0.25);
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum GameState {
+    #[default]
+    Title,
+    Loading,
+    Playing,
+    Lobby,
+    InGame,
 }
 
 // COMMMON COMPONENTS
@@ -17,28 +28,36 @@ pub struct ID(pub u16);
 pub struct Name(pub String);
 
 // COMMON RESOURCES
+#[derive(Resource, Default, Debug, Clone, Copy)]
+pub struct GameSeed(pub u64);
+
 #[derive(Resource)]
-pub struct GameSeed {
-    pub seed: u64,
+pub struct GlobalRng(pub ChaCha8Rng);
+
+// GLOBAL EVENTS
+#[derive(Event)]
+struct GenerateCard {
+    who: bool
 }
 
 // COMMMON SYSTEMS
-pub fn initialize_game_seed(mut commands: Commands) {
-    let mut rng = rand::rng();
-    let seed = rng.next_u64();
-    commands.insert_resource(GameSeed { seed });
+pub fn initialize_rng(mut commands: Commands) {
+    let random_seed = rand::random::<u64>();
+    commands.insert_resource(GameSeed(random_seed));
+    commands.insert_resource(GlobalRng(ChaCha8Rng::seed_from_u64(random_seed)));
 }
 
-
-pub fn gen_game_seed() -> u64 {
-    let mut rng = rand::rng();
-    rng.next_u64()
-}
-
-pub fn update_game_seed(mut commands: Commands, game_seed: Option<ResMut<GameSeed>>, seed: u64) {
-    if let Some(mut gs) = game_seed {
-        gs.seed = seed;
-    } else {
-        commands.insert_resource(GameSeed { seed });
+pub fn sync_rng_with_seed(
+    game_seed: Res<GameSeed>,
+    mut global_rng: ResMut<GlobalRng>
+) {
+    if game_seed.is_changed() {
+        println!("Seed alterada para: {}. Reiniciando o RNG.", game_seed.0);
+        *global_rng = GlobalRng(ChaCha8Rng::seed_from_u64(game_seed.0));
     }
 }
+
+pub fn get_host_card_id(mut rng: &mut ChaCha8Rng, cards: &[u16]) -> u16 {
+    cards.choose(rng).copied().unwrap_or_default()
+}
+
